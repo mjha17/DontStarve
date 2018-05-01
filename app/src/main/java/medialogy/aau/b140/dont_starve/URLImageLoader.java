04b5c37285c146c871c3e6409e330683a5c814b3
mjha17 <36537048+mjha17@users.noreply.github.com>
2018-05-01 12:47:57 +0200

    Added URLImageLoader class

---
+++ app/src/main/java/medialogy/aau/b140/dont_starve/URLImageLoader.java
@@ -0,0 +1,19 @@
+package medialogy.aau.b140.dont_starve;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by majoh on 01-05-2018.
+ */
+
+public class URLImageLoader extends AsyncTask<URL, int, Bitmap> {
+
+
+    @Override
+    protected Bitmap doInBackground(URL... urls) {
+        return null;
+    }
+}
