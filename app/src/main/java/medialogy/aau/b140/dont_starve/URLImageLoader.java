56cb31b9c0777fc6a553b6311d7034839083538f
mjha17 <36537048+mjha17@users.noreply.github.com>
2018-05-01 13:24:44 +0200

    Added AsyncResponse interface, implemented URLImageLoader.

--- app/src/main/java/medialogy/aau/b140/dont_starve/URLImageLoader.java
+++ app/src/main/java/medialogy/aau/b140/dont_starve/URLImageLoader.java
@@ -1,19 +1,49 @@
 package medialogy.aau.b140.dont_starve;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 /**
  * Created by majoh on 01-05-2018.
  */
 
-public class URLImageLoader extends AsyncTask<URL, int, Bitmap> {
+public class URLImageLoader extends AsyncTask<URL, Integer, Bitmap> {
 
+    private int id;
+    private BitmapFactory bf = new BitmapFactory();
+    private AsyncResponce responce;
+
+    private URLImageLoader(){}
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     @Override
     protected Bitmap doInBackground(URL... urls) {
-        return null;
+
+        Bitmap bitmap = null;
+
+        try {
+            InputStream is = urls[0].openStream();
+            bf.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+
+        responce.asyncResponce(bitmap, id);
+
     }
 }
