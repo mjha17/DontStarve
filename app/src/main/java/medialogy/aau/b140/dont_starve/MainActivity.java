447236b5000279fca150860cd2c9af97687f5e66
mjha17 <36537048+mjha17@users.noreply.github.com>
2018-05-01 13:29:13 +0200

    Added AsyncResponse interface, implemented URLImageLoader.

--- app/src/main/java/medialogy/aau/b140/dont_starve/MainActivity.java
+++ app/src/main/java/medialogy/aau/b140/dont_starve/MainActivity.java
@@ -1,13 +1,19 @@
 package medialogy.aau.b140.dont_starve;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AsyncResponse {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void asyncResponce(Bitmap bitmap, Int id) {
+        //set list item based on response id
     }
 }
