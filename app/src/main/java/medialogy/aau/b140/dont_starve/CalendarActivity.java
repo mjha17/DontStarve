5fecfd6d38f8160b79eb09a0b5813a0c96b5b3dd
Artimesh <nkris17@student.aau.dk>
2018-05-01 11:22:12 +0200

    Added the variables and methods to CalendarActivity

--- app/src/main/java/medialogy/aau/b140/dont_starve/CalendarActivity.java
+++ app/src/main/java/medialogy/aau/b140/dont_starve/CalendarActivity.java
@@ -1,29 +1,40 @@
 package medialogy.aau.b140.dont_starve;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    private Date[] plannedDays;
+    private Date currentDate = java.util.Date();
+    private RecipeListItem[] recipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void getRecipes(){
+
+    }
+    private void editEntries(){
+
+    }
+    private void toDateView(Date date){
+        //Switching between daily view and calendar view.
+    }
+    private void toMainScreen(){
+        Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
+        CalendarActivity.this.startActivity(intent);
     }
 
 }
