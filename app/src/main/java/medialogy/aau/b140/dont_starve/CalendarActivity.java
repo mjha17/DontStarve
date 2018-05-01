23a29dded07195afc05a2ba3f44cbe5cd129ca58
Artimesh <nkris17@student.aau.dk>
2018-05-01 11:38:54 +0200

    Added the variables and methods to CalendarActivity

--- app/src/main/java/medialogy/aau/b140/dont_starve/CalendarActivity.java
+++ app/src/main/java/medialogy/aau/b140/dont_starve/CalendarActivity.java
@@ -2,19 +2,14 @@ package medialogy.aau.b140.dont_starve;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
 
 import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
 
     private Date[] plannedDays;
-    private Date currentDate = java.util.Date();
+    private Date currentDate = new Date();
     private RecipeListItem[] recipes;
 
     @Override
