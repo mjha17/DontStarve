1c49229a289010ff9edc6d43e2ad1e89af3ca4a2
Artimesh <nkris17@student.aau.dk>
2018-05-02 13:27:28 +0200

    Fragments does not inflate properly. No idea why...

--- app/src/main/java/medialogy/aau/b140/dont_starve/MonthlyTab.java
+++ app/src/main/java/medialogy/aau/b140/dont_starve/MonthlyTab.java
@@ -1,13 +1,13 @@
 package medialogy.aau.b140.dont_starve;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MonthlyTab extends android.support.v4.app.Fragment {
+public class MonthlyTab extends Fragment {
 
     @Nullable
     @Override
