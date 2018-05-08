d5168ccfd1c92292f4141cfc668a2f57eb5c64a4
mjha17 <36537048+mjha17@users.noreply.github.com>
2018-05-08 14:36:42 +0200

    Changed the add ingredient button to be gray when nothing is in the text field

--- app/src/main/java/medialogy/aau/b140/dont_starve/MainActivity.java
+++ app/src/main/java/medialogy/aau/b140/dont_starve/MainActivity.java
@@ -1,8 +1,15 @@
 package medialogy.aau.b140.dont_starve;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.GetChars;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -82,6 +89,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 addIngredient();
+            }
+        });
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length() == 0){
+                    addButton.setBackgroundTintList(getResources().getColorStateList(R.color.darkGray));
+                }else{
+                    addButton.setBackgroundTintList(getResources().getColorStateList(R.color.positiveAction));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
